2908ace23fbcef468b6c86482f84696ba6c10802
Chris Bullock <cmabullock@gmail.com>
2013-06-02 23:32:41 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mlbstats::Application.config.secret_token = '1a38ad5d7c9ae2f831572d8dc467b68925be54bc29d9adfacfaca7bce0bfbe49913e6c43a4abf7038765d31213f4ff9306887c5fcaf2fec7333328f52e04f83e'
